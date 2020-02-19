46c29cc18afc23c172348c75ec02f2122eaa7b4d
fabcodingzest <fabrizvi786@gmail.com>
2020-02-19 16:25:28 +0530

    Add New Todo Functionality

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
 // Grabing elements from DOM
 const input = document.querySelector('.input');
 let ul = document.querySelector('.todo');
-let listItems = ul.getElementsByTagName('li');
-let deleteItem = document.querySelectorAll('.trash');
-
-const addTodo = () => {
-  alert("it works");
-}
+let list = document.querySelector('ul');
 
 
-// Do something when we press enter on input
-input.addEventListener('keypress', function (keypressed) {
-  if (keypressed.which === 13)
+//Click on trash icon
+function deleteTodo() {
+  let deleteItem = document.querySelectorAll('.trash');
+  for (item of deleteItem)
   {
-    addTodo()
+    item.addEventListener('click', function (event) {
+      this.parentNode.remove();
+      event.stopPropagation();
+    })
+  }
+}
+// Do something when we press enter on input
+input.addEventListener('keypress', function (event) {
+  if (event.which === 13)
+  {
+    //grab text from input
+    let todoText = this.value;
+    this.value = "";
+    // create a new li and add it to ul
+    let li = document.createElement("li");
+    let spanElement1 = document.createElement("span");
+    let spanElement2 = document.createElement("span");
+    let icon1 = document.createElement("i");
+    let icon2 = document.createElement("i");
+
+    icon1.classList.add("fas", "fa-pencil-alt");
+    spanElement1.append(icon1);
+    icon2.classList.add("fas", "fa-trash-alt");
+    spanElement2.classList.add("trash");
+    spanElement2.append(icon2);
+    if (todoText !== "") ul.appendChild(li).append(spanElement1, todoText, spanElement2);
+    completedTodo();
+    deleteTodo();
   }
 });
+sessionStorage.setItem('todoList', ul.innerHTML);
+
+function loadTodo() {
+  if (sessionStorage.getItem('todoList'))
+  {
+    ul.innerHTML = sessionStorage.getItem('todoList');
+    deleteTodo();
+  }
+}
+
+loadTodo();
 
 // When li is clicked
-for (li of listItems)
-{
-  li.addEventListener('click', function () {
-    if (this.classList.contains('completed'))
-    {
-      this.classList.remove('completed');
-    } else
-    {
-      this.classList.add('completed');
-    }
-  })
+function completedTodo() {
+  let listItems = ul.getElementsByTagName('li');
+  for (li of listItems)
+  {
+
+    li.addEventListener('click', function () {
+      if (this.classList.contains('completed'))
+      {
+        this.classList.remove('completed');
+      } else
+      {
+        this.classList.add('completed');
+      }
+    })
+  }
 }
 
-//Click on trash icon 
-for (item of deleteItem)
-{
-  item.addEventListener('click', function (event) {
-    event.stopPropagation();
-    this.parentNode.remove();
-  })
-}
